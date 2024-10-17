81580fcd7d8fd38329eed0a8745715d76c0bf660
David James Lynn <david.james.lynn@gmail.com>
2024-10-17 14:58:15 +0100

    Fixing up to work again

--- src/main/java/com/davidjlynn/countdownnumbers/service/model/operation/MultipleOperationSo.java
+++ src/main/java/com/davidjlynn/countdownnumbers/service/model/operation/MultipleOperationSo.java
@@ -1,8 +1,8 @@
-package com.davidjlynn.countdownnumbers.service.model.operation;
-
-import java.util.Set;
-
-public interface MultipleOperationSo extends OperationSo {
-
-  Set<NumberSo> getNumbers();
-}
+//package com.davidjlynn.countdownnumbers.service.model.operation;
+//
+//import java.util.Set;
+//
+//public interface MultipleOperationSo extends OperationSo {
+//
+//  Set<NumberSo> getNumbers();
+//}
